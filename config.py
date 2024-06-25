4e35d83473b0fb24cb6245ba5c806416065061e8
benjamin a <117926567+benarnav@users.noreply.github.com>
2024-06-25 15:44:27 -0400

    add weight init

--- config.py
+++ config.py
@@ -6,11 +6,12 @@ class GPT2Config:
     d_vocab: int = 50257
     d_seq: int = 1024
     d_model: int = 768
-    d_hidden: int = 3072
+    d_hidden: int = 3072  # 4 * d_model
     batch_size: int = 512
     epochs: int = 100
     d_head: int = 64  # d_model / num_heads
     num_heads: int = 12
     num_layers: int = 12
     dropout_rate: float = 0.01
+    weight_init: float = 0.02
     output_dir: str = "gpt2_output/"
