10b4c2e608dec1acc43d2b6906310ca76eb846b5
benjamin a <117926567+benarnav@users.noreply.github.com>
2024-06-25 16:32:27 -0400

    added init resid weight scaling

--- attention.py
+++ attention.py
@@ -15,6 +15,7 @@ class Attention(nn.Module):
         self.W_K = nn.Parameter(torch.randn((config.num_heads, config.d_model, config.d_head)) * math.sqrt(0.02))
         self.W_V = nn.Parameter(torch.randn((config.num_heads, config.d_model, config.d_head)) * math.sqrt(0.02))
         self.W_O = nn.Parameter(torch.randn((config.num_heads * config.d_head, config.d_model)) * math.sqrt(0.02))
+        self.W_O /= config.num_layers**0.5  # scaling initialization as specified in the paper
 
         self.b_Q = nn.Parameter(torch.randn((config.num_heads, config.d_head)) * math.sqrt(0.02))
         self.b_K = nn.Parameter(torch.randn((config.num_heads, config.d_head)) * math.sqrt(0.02))
@@ -25,17 +26,25 @@ class Attention(nn.Module):
         upper_tri = torch.triu(torch.ones((self.config.num_heads, self.config.d_seq, self.config.d_seq)), diagonal=1)
         self.mask = torch.where(upper_tri == 1, torch.tensor(float("-inf")), zeroes)
 
-    def forward(self, residual: torch.Tensor):  # shape ["batch", "seq", "d_model"]
-        K = einops.einsum(self.W_K, residual, "num_heads d_model d_head, batch seq d_model -> batch seq num_heads d_head")
+    def forward(self, residual: float[torch.Tensor, "batch seq d_model"]):  # shape ["batch", "seq", "d_model"]
+        K = einops.einsum(
+            self.W_K, residual, "num_heads d_model d_head, batch seq d_model -> batch seq num_heads d_head"
+        )
         K += self.b_K
 
-        Q = einops.einsum(self.W_Q, residual, "num_heads d_model d_head, batch seq d_model -> batch seq num_heads d_head")
+        Q = einops.einsum(
+            self.W_Q, residual, "num_heads d_model d_head, batch seq d_model -> batch seq num_heads d_head"
+        )
         Q += self.b_Q
 
-        V = einops.einsum(self.W_V, residual, "num_heads d_model d_head, batch seq d_model -> batch seq num_heads d_head")
+        V = einops.einsum(
+            self.W_V, residual, "num_heads d_model d_head, batch seq d_model -> batch seq num_heads d_head"
+        )
         V += self.b_V
 
-        QK = einops.einsum(Q, K, "batch seq_q num_heads d_head, batch seq_k num_heads d_head -> batch num_heads seq_q seq_k")
+        QK = einops.einsum(
+            Q, K, "batch seq_q num_heads d_head, batch seq_k num_heads d_head -> batch num_heads seq_q seq_k"
+        )
         QK_masked = QK + self.mask
 
         soft = torch.softmax((QK_masked / math.sqrt(self.config.d_head)), dim=-1)
