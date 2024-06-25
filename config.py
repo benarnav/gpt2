d92768ae4f77ba8fb86875f9b15750a94db92256
benjamin a <117926567+benarnav@users.noreply.github.com>
2024-06-25 13:50:38 -0400

    update batch size

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class GPT2Config:
     d_vocab: int = 50257
-    # d_pos: int = 50257
-    d_seq: int = 512
+    d_seq: int = 1024
     d_model: int = 768
     d_hidden: int = 3072
     batch_size: int = 512
